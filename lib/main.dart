9d4949e9c9e4c7a0583de99ffff0be93d15bd5bf
WangQing <wangqing11@100tal.com>
2019-11-22 12:47:40 +0800

    1. 添加 Tab，TabBar, TabBarView 的组合使用。

--- lib/main.dart
+++ lib/main.dart
@@ -27,41 +27,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("TabBar 和 TabBarView")),
+      body: Container(color: Colors.black, child: _bodyView()),
+    );
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget _bodyView() {
+    Widget bodyWidget = TabControllerPage();
+    return bodyWidget;
+  }
+}
+
+class TabControllerPage extends StatefulWidget {
+  @override
+  _TabControllerPageState createState() => _TabControllerPageState();
+}
+
+class _TabControllerPageState extends State<TabControllerPage>
+    with SingleTickerProviderStateMixin {
+  var _tabList = [
+    Tab(icon: Icon(Icons.looks_one), text: "第一页"),
+    Tab(icon: Icon(Icons.looks_two), text: "第二页"),
+  ];
+
+  var _tabBarViewList = [
+    Center(child: Text("第一页内容")),
+    Center(child: Text("第一1页内容")),
+    Center(child: Text("第一11页内容")),
+    Center(child: Text("第一111页内容")),
+    Center(child: Text("第二页内容"))
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: _tabList.length);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(color: Colors.white, child: _bodyView()),
+      backgroundColor: Colors.white30,
+      appBar: TabBar(controller: _tabController, tabs: _tabList),
+      body: TabBarView(controller: _tabController, children: _tabBarViewList),
     );
-  }
-
-  FlatButton flatButton(BuildContext context) {
-    return FlatButton(
-      textColor: Colors.white,
-      color: Theme.of(context).accentColor,
-      child: Text("FlatButton"),
-      onPressed: () {},
-    );
-  }
-
-  Widget _bodyView() {
-    Widget bodyWidget = PageView.custom(
-        scrollDirection: Axis.vertical,
-        childrenDelegate: SliverChildBuilderDelegate(
-          (BuildContext context, int index) {
-            return Center(child: Text("PageView custom 当前是第$index个"));
-          },
-          childCount: 10,
-        ));
-
-    return bodyWidget;
   }
 }
